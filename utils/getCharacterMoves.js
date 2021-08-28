c5263b49bef279bd013f6cd0c4baccb1bdf07319
aiacuone <aiacuone@users.noreply.github.com>
2021-08-28 19:51:40 +0100

    first commit

--- utils/getCharacterMoves.js
+++ utils/getCharacterMoves.js
@@ -6,8 +6,8 @@ export const getCharacterMoves = {
       })
     },
 
-    'START & FINISH': function ({ attr1: a, attr2: b, character }) {
-      const regex = new RegExp('^\\' + a + '.+' + '\\' + b + '\\s*$')
+    'START & FINISH': function ({ attr1: start, attr2: finish, character }) {
+      const regex = new RegExp('^\\' + start + '.+' + '\\' + finish + '\\s*$')
       return character.filter((move) => {
         return regex.test(move['Hit level'])
       })
@@ -24,103 +24,132 @@ export const getCharacterMoves = {
 
     COUNTER: function ({ character }) {
       let regex = /launch/i
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        if (regex.test(character[i]['Counter hit frame'])) {
-          arr.push(character[i])
-        }
-      }
-      return arr
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   if (regex.test(character[i]['Counter hit frame'])) {
+      //     arr.push(character[i])
+      //   }
+      // }
+      // return arr
+      return character.filter((move) => {
+        return regex.test(move['Counter hit frame'])
+      })
     },
   },
 
   FRAMES: {
     SPECIFIC: function ({ attr1: frames, character }) {
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        let filteredMove = character[i]['Start up frame'].replace('~', ' ')
-        if (filteredMove.substring(0, 2) === frames) {
-          arr.push(character[i])
-        }
-      }
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   let filteredMove = character[i]['Start up frame'].replace('~', ' ')
+      //   if (filteredMove.substring(0, 2) === frames) {
+      //     arr.push(character[i])
+      //   }
+      // }
 
-      return arr
+      // return arr
+
+      return character.filter((move) => {
+        return move['Start up frame'] === frames
+      })
     },
 
-    RANGE: function ({ attr1: min, attr2: max, character }) {
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        let filteredMove = character[i]['Start up frame'].replace('~', ' ')
-        if (
-          filteredMove.substring(0, 3) >= min &&
-          filteredMove.substring(0, 3) <= max
-        ) {
-          arr.push(character[i])
-        }
-      }
-      return arr
+    RANGE: function ({ attr1: min, attr2: max, character: moveList }) {
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   let filteredMove = character[i]['Start up frame'].replace('~', ' ')
+      //   if (
+      //     filteredMove.substring(0, 3) >= min &&
+      //     filteredMove.substring(0, 3) <= max
+      //   ) {
+      //     arr.push(character[i])
+      //   }
+      // }
+      // return arr
+      return moveList.filter((move) => {
+        const filteredMove = move['Start up frame']
+          .replace('~', ' ')
+          .substring(0, 3)
+        return filteredMove >= min && filteredMove <= max
+      })
     },
   },
 
   KNOCKDOWN: {
-    NORMAL: function ({ character }) {
-      let regex = /knd/i
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        if (regex.test(character[i]['Hit frame'])) {
-          arr.push(character[i])
-        }
-      }
+    NORMAL: function ({ character: moveList }) {
+      const regex = /knd/i
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   if (regex.test(character[i]['Hit frame'])) {
+      //     arr.push(character[i])
+      //   }
+      // }
 
-      return arr
+      // return arr
+      moveList.filter((move) => {
+        return regex.test(move['Hit frame'])
+      })
     },
 
-    COUNTER: function ({ character }) {
-      let regex = /knd/i
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        if (regex.test(character[i]['Counter hit frame'])) {
-          arr.push(character[i])
-        }
-      }
-      return arr
+    COUNTER: function ({ character: moveList }) {
+      const regex = /knd/i
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   if (regex.test(character[i]['Counter hit frame'])) {
+      //     arr.push(character[i])
+      //   }
+      // }
+      // return arr
+      moveList.filter((move) => {
+        return regex.test(move['Counter hit frame'])
+      })
     },
   },
 
   RAGE: {
-    'RAGE ART': function ({ character }) {
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        if (character[i]['Notes'] === 'Rage art') {
-          arr.push(character[i])
-        }
-      }
-      return arr
+    'RAGE ART': function ({ character: moveList }) {
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   if (character[i]['Notes'] === 'Rage art') {
+      //     arr.push(character[i])
+      //   }
+      // }
+      // return arr
+      moveList.filter((move) => {
+        return move['Notes'] === 'Rage art'
+      })
     },
 
     'RAGE DRIVE': function ({ character }) {
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        if (character[i]['Notes'] === 'Rage drive') {
-          arr.push(character[i])
-        }
-      }
-      return arr
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   if (character[i]['Notes'] === 'Rage drive') {
+      //     arr.push(character[i])
+      //   }
+      // }
+      // return arr
+      moveList.filter((move) => {
+        return move['Notes'] === 'Rage drive'
+      })
     },
   },
 
   SAFETY: {
-    SAFE: function ({ character }) {
-      let arr = []
-      for (let i = 0; i < character.length; i++) {
-        let filteredScript = character[i]['Block frame'].replace('~', ' ')
-        if (filteredScript.substring(0, 3) > -10) {
-          if (filteredScript !== '') {
-            arr.push(character[i])
-          }
-        }
-      }
-      return arr
+    SAFE: function ({ character: moveList }) {
+      // let arr = []
+      // for (let i = 0; i < character.length; i++) {
+      //   let filteredScript = character[i]['Block frame'].replace('~', ' ')
+      //   if (filteredScript.substring(0, 3) > -10) {
+      //     if (filteredScript !== '') {
+      //       arr.push(character[i])
+      //     }
+      //   }
+      // }
+      // return arr
+      moveList.filter((move) => {
+        const filteredMove = move['Block frame'].replace('~', ' ')
+        return filteredMove > -10
+      })
     },
 
     UNSAFE: function ({ character }) {
